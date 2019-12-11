49cd912b4fd4ee9d7c61918019ea821c7c79e7ad
Alejandro Adrian Arenas <aarenas@baufest.com>
2019-12-11 00:33:48 -0300

    se atrapan exceptions NoSePuedeUbicarPiezaEnSectorRival y NoSePuedeUbicarPorqueEstaOcupado y se lanza un popup al stage principal. NO IMPRIME MENSAJE, SOLO EL POPUP EN BLANCO!

--- AlgoChess/src/main/java/edu/fiuba/algoChess/interfaz/controlladores/UbicarPiezaHandler.java
+++ AlgoChess/src/main/java/edu/fiuba/algoChess/interfaz/controlladores/UbicarPiezaHandler.java
@@ -3,14 +3,20 @@ import edu.fiuba.algoChess.Modelo.bandos.Bando;
 import edu.fiuba.algoChess.Modelo.entidades.Pieza;
 import edu.fiuba.algoChess.Modelo.entorno.Tablero;
 import edu.fiuba.algoChess.Modelo.entorno.Ubicacion;
+import edu.fiuba.algoChess.Modelo.excepciones.NoSePuedeUbicarPiezaEnSectoRival;
+import edu.fiuba.algoChess.Modelo.excepciones.NoSePuedeUbicarPorqueEstaOcupadoException;
 import edu.fiuba.algoChess.Modelo.juego.Juego;
 import edu.fiuba.algoChess.interfaz.vista.MapView;
 import edu.fiuba.algoChess.interfaz.vista.PantallaPrincipal;
 import edu.fiuba.algoChess.interfaz.vista.PieceView;
 import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 
@@ -56,13 +62,49 @@ public class UbicarPiezaHandler implements EventHandler<ActionEvent> {
     public void handle(ActionEvent e) {
         int x = Integer.parseInt((tFX.getText()));
         int y = Integer.parseInt((tFY.getText()));
-        pieceView.setPieceMap(mapView, nombrePieza,"jugador1" , x, y);
+/*        Popup popup = new Popup();
+        HBox hbox = new HBox();
+        BorderPane bp = new BorderPane();
+        bp.setPrefSize(350,122);
+        bp.setStyle("-fx-background-color: #DBDBDF");
+        popup.setY(375);
+        popup.setX(520);
+*/
+        try {
+           pieceView.setPieceMap(mapView, nombrePieza,"jugador1" , x, y);
+       } catch(NoSePuedeUbicarPiezaEnSectoRival exc){
+            Label label1 = new Label("No se puede ubicar la pieza en una el sector rival");
+            Popup popup = new Popup();
+            HBox hbox = new HBox();
+            BorderPane bp = new BorderPane();
+            hbox.getChildren().add(label1);
+            bp.setAlignment(hbox, Pos.CENTER);
+            bp.setPrefSize(350,122);
+            bp.setStyle("-fx-background-color: #DBDBDF");
+            popup.setY(375);
+            popup.setX(520);
+            popup.getContent().addAll(bp);
+            popup.show(this.stagePrincipal);
+        } catch(NoSePuedeUbicarPorqueEstaOcupadoException ex){
+            Popup popup = new Popup();
+            HBox hbox = new HBox();
+            BorderPane bp = new BorderPane();
+            Label label = new Label("No se puede ubicar la pieza en una celda ocupada");
+            hbox.getChildren().add(label);
+            bp.setAlignment(hbox, Pos.CENTER);
+            bp.setPrefSize(350,122);
+            bp.setStyle("-fx-background-color: #DBDBDF");
+            popup.setY(375);
+            popup.setX(520);
+            popup.getContent().addAll(bp);
+            popup.show(this.stagePrincipal);
+        }
         pantallaPrincipal.cambioTurno(head, nombrePieza);
         stageUbicar.close();
-        Class piezaClass = listaPieza.get(nombrePieza);
+/*        Class piezaClass = listaPieza.get(nombrePieza);
         Ubicacion ubicacion = new Ubicacion(x,y);
         Tablero tablero = juego.getTablero();
-  /*      try {
+        try {
             try {
                 Pieza pieza = (Pieza) piezaClass.getDeclaredConstructor().newInstance();
             } catch (InvocationTargetException ex) {
